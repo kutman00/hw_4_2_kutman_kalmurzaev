def860f1d82760dd2b4af6f91b3bfea8f467a08a
kutman00 <kutmankalmurzaev45@gmail.com>
2022-04-11 18:14:22 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> color = new ArrayList<String>(3);
+        color.add("Red");
+        color.add("Green");
+        color.add("Black");
+        color.ensureCapacity( 6);
+        color.add("White");
+        color.add("Pink");
+        color.add("Yellow");
+        Collections.reverse(color);
+        Collections.shuffle(color);
+        System.out.println(color);
+
+
+    }
+}
